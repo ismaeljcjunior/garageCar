b9e238ffc50ef2143b44ad6239260fd9342d0ab0
ismaeljcjunior <ismaeljcjunior@gmail.com>
2022-06-26 21:16:58 -0300

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,51 @@
+
+import React, { useCallback, useEffect, useState } from 'react';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo'
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components'
+import theme from './src/styles/theme';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  // const [appIsReady, setAppIsReady] = useState(false);
+  // useEffect(() => {
+  //   async function prepare() {
+  //     try {
+  //       await SplashScreen.preventAutoHideAsync();
+  //       await Font.loadAsync({
+  //         Inter_400Regular, Inter_500Medium, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold
+
+  //       });
+  //     } catch (e) {
+  //       console.warn(e);
+  //     } finally {
+  //       setAppIsReady(true);
+  //     }
+  //   }
+
+  //   prepare();
+  // }, [])
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (appIsReady) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [appIsReady]);
+
+  // if (!appIsReady) {
+  //   return null;
+  // }
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+
+  );
+}
+
+
